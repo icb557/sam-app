b53fec0538a43adf5746899c9265541785b50a91
cbisac <cortesbuitragoisac@gmail.com>
2025-01-15 18:34:22 -0500

    Changed return message

---
+++ LoadTestForCanaryDeployment.ps1
@@ -0,0 +1,19 @@
+﻿# Retrieve the DEV_ENDPOINT
+$DEV_ENDPOINT = (aws cloudformation describe-stacks --stack-name sam-app-dev | ConvertFrom-Json).Stacks.Outputs | Where-Object { $_.OutputValue -like "https://*" } | Select-Object -ExpandProperty OutputValue
+
+# Retrieve the PROD_ENDPOINT
+$PROD_ENDPOINT = (aws cloudformation describe-stacks --stack-name sam-app-prod | ConvertFrom-Json).Stacks.Outputs | Where-Object { $_.OutputValue -like "https://*" } | Select-Object -ExpandProperty OutputValue
+
+while ($true) {
+    $response = curl -s $DEV_ENDPOINT | ConvertFrom-Json
+    $message = $response.message
+    $message | Out-File -Append -FilePath outputs.txt
+    Write-Output $message
+
+    Start-Sleep -Seconds 1
+
+    # Exit loop if "q" is pressed
+    if ([console]::KeyAvailable -and [console]::ReadKey($true).Key -eq 'Q') {
+        break
+    }
+}
